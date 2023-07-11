f9659559d9e808a9da69bad799f2c492d76f17e7
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2023-07-11 17:20:41 +0800

    test: sample noExceptionThrown test

---
+++ src/test/java/com/mmfallacy/GradleTutorialTest.java
@@ -0,0 +1,11 @@
+package com.mmfallacy;
+
+import org.junit.Test;
+
+public class GradleTutorialTest {
+
+    @Test
+    public void verifyNoExceptionThrown() {
+        GradleTutorial.main(new String[] {});
+    }
+}
